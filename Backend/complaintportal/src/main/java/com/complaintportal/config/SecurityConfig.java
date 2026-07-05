e92ab5ac5b237c14ef6c1228e2abf5eca64ae409
Jatinmourya24932 <Jatinmourya24932@gmail.com>
2026-07-05 11:03:05 +0530

    CORS update

--- Backend/complaintportal/src/main/java/com/complaintportal/config/SecurityConfig.java
+++ Backend/complaintportal/src/main/java/com/complaintportal/config/SecurityConfig.java
@@ -77,8 +77,9 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "http://localhost:5173"
+//                "http://localhost:3000",
+//                "http://localhost:5173",
+                "https://complaint-portal-2.vercel.app/"
         ));
 
         configuration.setAllowedMethods(List.of("*"));
